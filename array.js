3986d217b0c148e7eb80a4e5a2f9017759f7a4a1
brunolindao <brunoayres365@gmail.com>
2025-06-10 20:18:00 -0300

    array

--- array.js
+++ array.js
@@ -12,9 +12,15 @@ let idades = ['12', '25', '24'];
 //console.log(carros);
 //carros.push("ferrari");
 //console.log(carros);
- console.log(nomes);
+ /*console.log(nomes);
  nomes.pop()
  console.log(nomes);
  nomes.shift()
  console.log('nomes'.length);
- console.log(`${nomes[1]} ${nomes[2]}´);
+ console.log(`${nomes[1]} ${nomes[2]}´);*/
+
+ let frutas = ["maçã", "pera", "banana", "limão", "uva"];
+
+frutas.forEach(function(fruta){
+    console.log(fruta);
+})
