d6d97f9d0335c9076d2856a2f213a5ea41e9fb62
brunolindao <brunoayres365@gmail.com>
2025-06-10 21:30:47 -0300

    testando

---
+++ meuobjeto.js
@@ -0,0 +1,17 @@
+let curso = {
+
+capacidade: 40,
+duraçao: '6h',
+codigo: 'fs25',
+professor: 'angelo',
+turno: 'manha',
+preço: 7000,
+sala: {
+    cadeiras: 30,
+    pc: true,
+    projetor: true,
+    nome : "grace hoper"
+}
+}
+
+console.log(curso.sala);
