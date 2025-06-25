6adbf620e15f46dc99fda74d9f4f361b93817d40
brunolindao <brunoayres365@gmail.com>
2025-06-24 22:06:53 -0300

    msg

--- script.js
+++ script.js
@@ -11,5 +11,14 @@ console.log(`URI: ${uri}`)
 
 fetch(uri)
 .then(response => response.json())
-.then(json => console.log(json))
+.then(json => {
+    console.log(json)
+document.getElementById('logradouro').value = json.address
+
+document.getElementById('bairro').value = json.district
+document.getElementById('uf').value = json.state
+document.getElementById('localidade').value = json.city
+document.getElementById('ddd').value = json.ddd
+document.getElementById('uf').value = json.state
+})
 }
